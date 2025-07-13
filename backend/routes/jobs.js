38939e53c0eec7717482e6594b292cd988a53640
nehagarg-123 <nehagarg6201@gmail.com>
2025-07-13 20:00:47 +0530

    Update jobs.js

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -8,10 +8,10 @@ const {
   deleteJob,
 } = require('../controllers/jobs')
 
-router.get('/', getAllJobs) // using get api frontend can take data 
+router.get('/', getAllJobs) // using get api, frontend can take data 
 router.get('/:id', getJob)
-router.post('/', createJob)// using post api we create like job
-router.patch('/:id', updateJob)// using update api we update the data
-router.delete('/:id', deleteJob) // using delete api we delete the data
+router.post('/', createJob)// using post api ,we create like job
+router.patch('/:id', updateJob)// using update api, we update the data
+router.delete('/:id', deleteJob) // using delete api, we delete the data
 
 module.exports = router   //exports the router so you can plug it into your server.js file.
