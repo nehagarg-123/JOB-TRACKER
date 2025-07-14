594fd3f3ea1d7022ae637b757dcfe749d9aa3a0b
nehagarg-123 <nehagarg6201@gmail.com>
2025-07-14 22:42:22 +0530

    Update custom-api.js

--- backend/errors/custom-api.js
+++ backend/errors/custom-api.js
@@ -1,7 +1,7 @@
 class CustomAPIError extends Error {
   // here creating own error class that extend the built in error class in js
   constructor(message) {// using constructor->it receives an error message when you throw it (like 'job not found')
-    super(message)// the passes the msg to the base error class so it behaves like a real errpr
+    super(message)// the passes the msg to the base error class so it behaves like a real error
   }
 }
 
