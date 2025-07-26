e04498933acedb4477064d94847655028ca137eb
nehagarg-123 <nehagarg6201@gmail.com>
2025-07-26 12:00:53 +0530

    Update JobCard.jsx

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -2,6 +2,10 @@ import { useNavigate } from 'react-router-dom'
 
 const JobCard = ({ job }) => {
   const navigate = useNavigate()
+
+  const formattedDate = job.statusDate
+    ? new Date(job.statusDate).toLocaleDateString()
+    : 'Not set'
   return (
     <div
       className="bg-light text-center p-3 rounded-2"
@@ -10,6 +14,7 @@ const JobCard = ({ job }) => {
       <p>Company: {job.company}</p>
       <p>Position: {job.position}</p>
       <p>Status: {job.status}</p>
+      <p>Date: {formattedDate}</p>
       <button
         onClick={() => navigate(`/jobs/${job._id}/edit`)}
         className="btn btn-md w-75 btn-dark"
