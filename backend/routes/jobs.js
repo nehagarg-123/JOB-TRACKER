5df13f37692da346faf66b7641ece9039f7533e8
nehagarg-123 <nehagarg6201@gmail.com>
2025-11-18 23:23:16 +0530

    Update jobs.js

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -8,12 +8,12 @@ const {
   deleteJob,
 } = require('../controllers/jobs')
 
-const upload = require('../middlewares/multer');
+
 
 router.get('/', getAllJobs) // using get api, frontend can take data 
 router.get('/:id', getJob)
 router.post('/', createJob)// using post api ,we create like job
-router.patch('/:id', upload.single('resume'), updateJob);// using update api, we update the data
+router.patch('/:id', updateJob);// using update api, we update the data
 router.delete('/:id', deleteJob) // using delete api, we delete the data
 
 module.exports = router   //exports the router so you can plug it into your server.js file.
