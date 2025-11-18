0ddd2b7e2d105a0f607c308ced55add1684a7b18
nehagarg-123 <nehagarg6201@gmail.com>
2025-11-18 23:06:16 +0530

    Delete backend/middlewares/multer.js

--- backend/middlewares/multer.js
+++
@@ -1,9 +0,0 @@
-// middleware/multer.js
-const multer = require('multer');
-
-// We'll store the file in memory temporarily
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage: storage });
-
-module.exports = upload;
