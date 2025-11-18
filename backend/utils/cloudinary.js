444e96becab3e4489778168aa40f59fb94993ed9
nehagarg-123 <nehagarg6201@gmail.com>
2025-11-18 16:19:51 +0530

    Create cloudinary.js

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,11 @@
+// utils/cloudinary.js
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config(); // Make sure .env variables are loaded
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+module.exports = cloudinary;
